1b06043e81b48257ef79d96dd9bce9f36be914c8
MarinaYano <myano2621@gmail.com>
2024-07-13 01:04:28 -0700

    update: include image data

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -8,27 +8,48 @@ import { CreateBoard } from "./schema";
 import { createSafeAction } from "@/lib/create-safe-action";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
-  const { userId } = auth();
+  const { userId, orgId } = auth();
 
-  if(!userId) {
+  if(!userId || !orgId) {
     return {
       error: "Unauthorized",
     }
   }
 
-  const { title } = data;
+  const { title, image } = data;
+
+  const [
+    imageId,
+    imageThumbUrl,
+    imageFullUrl,
+    imageLinkHTML,
+    imageUserName,
+  ] = image.split("|");
+
+  if(!title || !imageId || !imageThumbUrl || !imageFullUrl || !imageLinkHTML || !imageUserName) {
+    return {
+      error: "Invalid image data",
+    }
+  }
+
   let board;
 
   try {
     board = await db.board.create({
       data: {
+        orgId: orgId,
         title,
+        imageId,
+        imageThumbUrl,
+        imageFullUrl,
+        imageUserName,
+        imageLinkHTML,
       }
     })
   } catch (error) {
     return {
       error: "Failed to create",
-    }    
+    }
   }
 
   revalidatePath(`/board/${board.id}`);
