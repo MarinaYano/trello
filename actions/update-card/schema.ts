e97bbdab18a1f4f4aee506df2cdfec62a87a6957
MarinaYano <myano2621@gmail.com>
2024-07-16 15:43:46 -0700

    fix: change validation for title as optional item

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const UpdateCard = z.object({
   boardId: z.string(),
-  Description: z.optional(
+  description: z.optional(
     z.string({
       required_error: "Description is required",
       invalid_type_error: "Description must be a string",
@@ -11,11 +11,13 @@ export const UpdateCard = z.object({
       message: "Description must be at least 3 characters",
     })
   ),
-  title: z.string({
-    required_error: "Title is required",
-    invalid_type_error: "Title must be a string",
-  }).min(3, {
-    message: "Title must be at least 3 characters",
-  }),
+  title: z.optional(
+    z.string({
+      required_error: "Title is required",
+      invalid_type_error: "Title must be a string",
+    }).min(3, {
+      message: "Title must be at least 3 characters",
+    })
+  ),
   id: z.string(),
 });
